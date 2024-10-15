47f0af036277e201ba2d2dd04748bf4af5733995
hgsk <hagihararyosuke@gmail.com>
2024-10-16 08:47:58 +0900

    Update SceneTransitionManager.cs

--- Scene/SceneTransitionManager.cs
+++ Scene/SceneTransitionManager.cs
@@ -1,5 +1,30 @@
 using UnityEngine;
 using UnityEngine.Playables;
+/*
+1. 新しい空のゲームオブジェクトを作成し、「SceneTransitionManager」という名前を付けます。
+
+2. SceneTransitionManagerスクリプトをアタッチします。
+
+3. 同じゲームオブジェクトにCoroutineRunnerスクリプトもアタッチします。
+
+4. Unityのタイムラインウィンドウを開き、インとアウトのトランジション用に2つの新しいタイムラインアセットを作成します。
+
+5. 各タイムラインで、アニメーショントラックを追加し、フェードイン/フェードアウトなどのトランジション効果を作成します。
+
+6. SceneTransitionManagerのInTransitionとOutTransitionフィールドに、それぞれ対応するタイムラインアセットをドラッグ＆ドロップします。
+
+7. SceneManagerSOアセットで、transitionDurationをトランジションの長さに合わせて調整します。
+
+これにより、デザイナーは以下のようにタイムラインを使ってビジュアル的にトランジションを作成・編集できるようになります：
+
+- フェードイン/アウト効果
+- ワイプトランジション
+- カットアウトアニメーション
+- パーティクル効果
+- その他のビジュアルエフェクト
+
+デザイナーは、Unityのタイムラインエディタを使用してこれらのエフェクトを作成・調整できます。
+*/
 
 public class SceneTransitionManager : MonoBehaviour
 {
