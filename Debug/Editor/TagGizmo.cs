c27b9519579893e2531ccd4c52fd82b3372287bf
hgsk <hagihararyosuke@gmail.com>
2024-10-15 09:23:55 +0900

    Create TagGizmo.cs

---
+++ Debug/Editor/TagGizmo.cs
@@ -0,0 +1,32 @@
+/*
+1. `[InitializeOnLoad]` 属性により、Unityエディタの起動時にこのクラスが初期化されます。
+
+2. 静的コンストラクタで `SceneView.duringSceneGui` イベントに `OnSceneGUI` メソッドを登録しています。
+
+3. `OnSceneGUI` メソッドでは、シーン内のすべてのGameObjectを走査し、タグが設定されているオブジェクトに対してそのタグを表示します。
+
+4. `Handles.Label` を使用して、オブジェクトの位置にタグのテキストを描画します。
+*/
+
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class TagGizmo
+{
+    static TagGizmo()
+    {
+        SceneView.duringSceneGui += OnSceneGUI;
+    }
+
+    static void OnSceneGUI(SceneView sceneView)
+    {
+        foreach (GameObject obj in GameObject.FindObjectsOfType<GameObject>())
+        {
+            if (!string.IsNullOrEmpty(obj.tag) && obj.tag != "Untagged")
+            {
+                Handles.Label(obj.transform.position, obj.tag);
+            }
+        }
+    }
+}
